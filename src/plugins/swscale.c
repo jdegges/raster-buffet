f567533c172603354384700f5b573faa414e8805
Joey Degges <jdegges@gmail.com>
2010-04-11 01:08:50 -0700

    Fixed up the swscale plugin so that images can be converted into RGB24.

--- src/plugins/swscale.c
+++ src/plugins/swscale.c
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <strings.h>
+#include <assert.h>
 
 #include <unistd.h>
 #include <pthread.h>
@@ -37,6 +38,25 @@
 
 #include <image.h>
 #include <plugin.h>
+
+#define error_exit(func, msg) { \
+    fprintf (stderr, "%s_%s(): %s\n", decode_name, func, msg); \
+    ret_val = -1; \
+    goto exit; \
+}
+
+typedef struct swscale_decode_context {
+    struct async_queue* sws_context_queue;
+    int references;
+
+    int dst_width;
+    int dst_height;
+    data_fmt dst_native_fmt;
+    enum PixelFormat dst_sws_fmt;
+} swscale_decode_context;
+
+static enum PixelFormat supported_fmt_table[FMT_LIST] = {-1};
+
 
 /* function definitions */
 int swscale_query (plugin_stage   stage,
@@ -52,8 +72,9 @@ int swscale_decode_exec (plugin_context*  ctx,
 int swscale_decode_exit (plugin_context*  ctx,
                          int              thread_id);
 
+
 /* decode plugin configuration */
-static const char decode_name[] = "rb_swscale_decode";
+static const char decode_name[] = "swscale_decode";
 static const data_fmt decode_src_fmt[] = {FMT_LIST, -1};
 static const data_fmt decode_dst_fmt[] = {
     FMT_BMP,    FMT_CUT,    FMT_DDS,
@@ -77,6 +98,7 @@ static plugin_info pi_swscale_decode = {.stage=PLUGIN_STAGE_INPUT,
                                         .exit=swscale_decode_exit,
                                         .exec=swscale_decode_exec};
 
+
 int swscale_query (plugin_stage stage, plugin_info** pi)
 {
     *pi = NULL;
@@ -90,197 +112,7 @@ int swscale_query (plugin_stage stage, plugin_info** pi)
     return 0;
 }
 
-typedef struct swscale_decode_context {
-    struct async_queue* sws_context_queue;
-
-/*
-    int src_width;
-    int src_height;
-    data_fmt src_native_fmt;
-    enum PixelFormat src_sws_fmt;
-*/
-
-    int dst_width;
-    int dst_height;
-    data_fmt dst_native_fmt;
-    enum PixelFormat dst_sws_fmt;
-} swscale_decode_context;
-
-
-static enum PixelFormat supported_fmt_table[FMT_LIST] = {-1};
-
-
-int swscale_decode_init (plugin_context*  ctx,
-                         int              thread_id,
-                         char*            args)
-{
-    swscale_decode_context* c;
-    struct SwsContext* sws_context;
-
-    pthread_mutex_lock (&ctx->mutex);
-
-    if (ctx->data == NULL) {
-        char* param;
-        data_fmt dst_fmt;
-        enum PixelFormat sws_fmt;
-        int dst_width;
-        int dst_height;
-
-        /* set up state shared between all threads */
-        if (NULL == (c = malloc (sizeof *c))) {
-            pthread_mutex_unlock (&ctx->mutex);
-            return -1;
-        }
-
-        if (NULL == (c->sws_context_queue = async_queue_new())) {
-            pthread_mutex_unlock (&ctx->mutex);
-            return -1;
-        }
-
-        parse_args (args, 0, "width",  &param);
-        if (NULL == param || (dst_width = atoi (param)) <= 0) {
-            dst_width = -1;
-        }
-
-        parse_args (args, 0, "height", &param);
-        if (NULL == param || (dst_height = atoi (param)) <= 0) {
-            dst_height = -1;
-        }
-
-        parse_args (args, 0, "fmt", &param);
-        if (NULL == param || stofmt (param, &dst_fmt, &sws_fmt) < 0) {
-            dst_fmt = FMT_RGB24;
-            sws_fmt = PIX_FMT_RGB24;
-        }
-
-        c->dst_width = dst_width;
-        c->dst_height = dst_height;
-        c->dst_native_fmt = dst_fmt;
-        c->dst_sws_fmt = sws_fmt;
-        ctx->data = c;
-    }
-
-    /* setup state for this thread only (in this case each thread allocates its
-     * own fi_input_context.buf buffer) */
-    if (NULL == (c = (swscale_decode_context*) ctx->data) ||
-        NULL != c->sws_context_queue)
-    {
-        pthread_mutex_unlock (&ctx->mutex);
-        return -1;
-    }
-
-supported_fmt_table[FMT_NONE] = PIX_FMT_NONE;
-supported_fmt_table[FMT_PAL8] = PIX_FMT_PAL8;
-supported_fmt_table[FMT_BGR24] = PIX_FMT_BGR24;
-supported_fmt_table[FMT_BGR32] = PIX_FMT_BGR32;
-supported_fmt_table[FMT_BGR32_1] = PIX_FMT_BGR32_1;
-supported_fmt_table[FMT_RGB24] = PIX_FMT_RGB24;
-supported_fmt_table[FMT_RGB32] = PIX_FMT_RGB32;
-supported_fmt_table[FMT_RGB32_1] = PIX_FMT_RGB32_1;
-supported_fmt_table[FMT_YUYV] = PIX_FMT_YUYV422;
-supported_fmt_table[FMT_UYVY] = PIX_FMT_UYVY422;
-
-
-
-    pthread_mutex_unlock (&ctx->mutex);
-
-    return 0;
-}
-
-int swscale_decode_exec (plugin_context*  ctx,
-                         int              thread_id,
-                         image_t**        src_data,
-                         image_t**        dst_data)
-{
-    swscale_decode_context* c;
-    struct SwsContext* sws_context;
-    AVPicture src_picture;
-    AVPicture dst_picture;
-
-    image_t* sim;
-    uint8_t* pix;
-
-    int dst_width;
-    int dst_height;
-
-    if (NULL == (sim = *src_data) || NULL != *dst_data) {
-        return -1;
-    }
-
-    if (NULL == (sws_context = async_queue_pop (c->sws_context_queue, false)))
-    {
-        if (c->dst_width <= 0 || c->dst_height <= 0) {
-            dst_width = sim->width;
-            dst_height = sim->height;
-        } else {
-            dst_width = c->dst_width;
-            dst_height = c->dst_height;
-        }
-
-        sws_context = sws_getContext (sim->width, sim->height, native_to_sws (sim->fmt),
-                                      dst_width, dst_height, c->dst_sws_fmt,
-                                      SWS_BICUBIC, NULL, NULL, NULL);
-        if (NULL == sws_context) {
-            fprintf (stderr, "Error creating sws_context :/\n");
-            return -1;
-        }
-        
-    }
-
-    if (NULL == (pix = malloc ((sizeof *pix) * dst_width * dst_height * 3))) {
-        return -1;
-    }
-
-    avpicture_fill (&src_picture, sim->pix, native_to_sws (sim->fmt), sim->width, sim->height);
-    avpicture_fill (&dst_picture, pix, c->dst_sws_fmt, dst_width, dst_height);
-
-    if (dst_height != sws_scale (sws_context,
-                                 (const uint8_t**) src_picture.data, src_picture.linesize,
-                                 0, dst_height,
-                                 dst_picture.data, dst_picture.linesize))
-    {
-        fprintf (stderr, "sws_scale returned a value other than: %d\n", dst_height);
-        return -1;
-    }
-
-    if (sim->ext_data && sim->ext_free) {
-        sim->ext_free (sim->ext_data);
-        sim->ext_data = NULL;
-        sim->ext_free = NULL;
-    } else {
-        free (sim->pix);
-    }
-
-    sim->fmt = c->dst_native_fmt;
-    sim->pix = pix;
-
-    *dst_data = sim;
-    *src_data = NULL;
-
-
-    return 0;
-}
-
-/*
-static const enum PixelFormat supported_fmt_table[FMT_LIST] = {-1};
-supported_fmt_table[FMT_NONE] = PIX_FMT_NONE;
-supported_fmt_table[FMT_PAL8] = PIX_FMT_PAL8;
-supported_fmt_table[FMT_BGR24] = PIX_FMT_BGR24;
-supported_fmt_table[FMT_BGR32] = PIX_FMT_BGR32;
-supported_fmt_table[FMT_BGR32_1] = PIX_FMT_BGR32_1;
-supported_fmt_table[FMT_RGB24] = PIX_FMT_RGB24;
-supported_fmt_table[FMT_RGB32] = PIX_FMT_RGB32;
-supported_fmt_table[FMT_RGB32_1] = PIX_FMT_RGB32_1;
-supported_fmt_table[FMT_YUYV] = PIX_FMT_YUYV422;
-supported_fmt_table[FMT_UYVY] = PIX_FMT_UYVY422;
-*/
-
-enum PixelFormat native_to_sws (data_fmt fmt)
-{
-    return supported_fmt_table[fmt];
-}
-
-int stofmt (char* str, data_fmt* native_fmt, enum PixelFormat* sws_fmt)
+static int stofmt (char* str, data_fmt* native_fmt, enum PixelFormat* sws_fmt)
 {
     if (NULL == str) {
         *native_fmt = FMT_NONE;
@@ -312,4 +144,191 @@ int stofmt (char* str, data_fmt* native_fmt, enum PixelFormat* sws_fmt)
     *sws_fmt = supported_fmt_table[*native_fmt];
 
     return 0;
+}
+
+int swscale_decode_init (plugin_context*  ctx,
+                         int              thread_id,
+                         char*            args)
+{
+    swscale_decode_context* c;
+    struct SwsContext* sws_context;
+    int ret_val = -1;
+
+    pthread_mutex_lock (&ctx->mutex);
+
+    if (ctx->data == NULL) {
+        char* param;
+        data_fmt dst_fmt;
+        enum PixelFormat sws_fmt;
+        int dst_width;
+        int dst_height;
+
+        /* set up state shared between all threads */
+        if (NULL == (c = calloc (1, sizeof *c))) {
+            error_exit ("init", "out of memory");
+        }
+
+        if (NULL == (c->sws_context_queue = async_queue_new())) {
+            error_exit ("init", "failed to create async queue");
+        }
+
+        parse_args (args, 0, "width",  &param);
+        if (NULL == param || (dst_width = atoi (param)) <= 0) {
+            dst_width = -1;
+        }
+
+        parse_args (args, 0, "height", &param);
+        if (NULL == param || (dst_height = atoi (param)) <= 0) {
+            dst_height = -1;
+        }
+
+        parse_args (args, 0, "fmt", &param);
+        if (NULL == param || stofmt (param, &dst_fmt, &sws_fmt) < 0) {
+            dst_fmt = FMT_RGB24;
+            sws_fmt = PIX_FMT_RGB24;
+        }
+
+        c->dst_width = dst_width;
+        c->dst_height = dst_height;
+        c->dst_native_fmt = dst_fmt;
+        c->dst_sws_fmt = sws_fmt;
+        ctx->data = c;
+
+        supported_fmt_table[FMT_NONE] = PIX_FMT_NONE;
+        supported_fmt_table[FMT_PAL8] = PIX_FMT_PAL8;
+        supported_fmt_table[FMT_BGR24] = PIX_FMT_BGR24;
+        supported_fmt_table[FMT_BGR32] = PIX_FMT_BGR32;
+        supported_fmt_table[FMT_BGR32_1] = PIX_FMT_BGR32_1;
+        supported_fmt_table[FMT_RGB24] = PIX_FMT_RGB24;
+        supported_fmt_table[FMT_RGB32] = PIX_FMT_RGB32;
+        supported_fmt_table[FMT_RGB32_1] = PIX_FMT_RGB32_1;
+        supported_fmt_table[FMT_YUYV] = PIX_FMT_YUYV422;
+        supported_fmt_table[FMT_UYVY] = PIX_FMT_UYVY422;
+    }
+
+    if (NULL == (c = (swscale_decode_context*) ctx->data) ||
+        NULL == c->sws_context_queue)
+    {
+        error_exit ("init", "context is not properly set");
+    }
+
+    c->references++;
+    ret_val = 0;
+
+    pthread_mutex_unlock (&ctx->mutex);
+
+exit:
+    return ret_val;
+}
+
+
+int swscale_decode_exit (plugin_context*  ctx,
+                         int              thread_id)
+{
+    swscale_decode_context* c;
+
+    pthread_mutex_lock (&ctx->mutex);
+
+    assert (NULL != (c = ctx->data));
+
+    if (!--c->references) {
+        struct SwsContext* sws_context;
+        while (NULL != (sws_context = async_queue_pop (c->sws_context_queue,
+                                                       false)))
+        {
+            sws_freeContext (sws_context);
+        }
+
+        free (c);
+        ctx->data = NULL;
+    }
+
+    pthread_mutex_unlock (&ctx->mutex);
+     return 0;
+}
+
+static enum PixelFormat native_to_sws (data_fmt fmt)
+{
+    return supported_fmt_table[fmt];
+}
+
+int swscale_decode_exec (plugin_context*  ctx,
+                         int              thread_id,
+                         image_t**        src_data,
+                         image_t**        dst_data)
+{
+    swscale_decode_context* c;
+    struct SwsContext* sws_context;
+    AVPicture src_picture;
+    AVPicture dst_picture;
+    int ret_val = -1;
+
+    image_t* sim;
+    uint8_t* pix;
+
+    int dst_width;
+    int dst_height;
+
+    if (NULL == (sim = *src_data) || NULL != *dst_data) {
+        error_exit ("exec", "bad src/dst data pointers");
+    }
+
+    assert (NULL != (c = ctx->data));
+    assert (NULL != c->sws_context_queue);
+
+    if (c->dst_width <= 0 || c->dst_height <= 0) {
+        dst_width = sim->width;
+        dst_height = sim->height;
+    } else {
+        dst_width = c->dst_width;
+        dst_height = c->dst_height;
+    }
+
+    if (NULL == (sws_context = async_queue_pop (c->sws_context_queue, false)))
+    {
+        sws_context = sws_getContext (sim->width, sim->height, native_to_sws (sim->fmt),
+                                      dst_width, dst_height, c->dst_sws_fmt,
+                                      SWS_BICUBIC, NULL, NULL, NULL);
+        if (NULL == sws_context) {
+            error_exit ("exec", "error creating sws_context");
+        }
+        
+    }
+
+    if (NULL == (pix = malloc ((sizeof *pix) * dst_width * dst_height * 3))) {
+        error_exit ("exec", "out of memory");
+    }
+
+    avpicture_fill (&src_picture, sim->pix, native_to_sws (sim->fmt), sim->width, sim->height);
+    avpicture_fill (&dst_picture, pix, c->dst_sws_fmt, dst_width, dst_height);
+
+    if (dst_height != sws_scale (sws_context,
+                                 (const uint8_t**) src_picture.data, src_picture.linesize,
+                                 0, dst_height,
+                                 dst_picture.data, dst_picture.linesize))
+    {
+        error_exit ("exec", "sws_scale failed to convert src->dst");
+    }
+
+    assert (async_queue_push (c->sws_context_queue, sws_context));
+
+    if (sim->ext_data && sim->ext_free) {
+        sim->ext_free (sim->ext_data);
+        sim->ext_data = NULL;
+        sim->ext_free = NULL;
+    } else {
+        free (sim->pix);
+    }
+
+    sim->fmt = c->dst_native_fmt;
+    sim->pix = pix;
+    sim->bpp = 24;
+
+    *dst_data = sim;
+    *src_data = NULL;
+
+    ret_val = 0;
+
+exit:
+    return ret_val;
 }
